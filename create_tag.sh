161d1476f9cd68838641b048f26b3f74a3a07112
ruoce <sky_ruocen@qq.com>
2025-08-01 18:09:31 +0800

    fix bug

---
+++ create_tag.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 定义要创建的标签
+TAG_NAME="v0.1.1"
+
+# 创建本地标签
+git tag $TAG_NAME
+# 推送标签到远程仓库
+git push origin $TAG_NAME
+
+# 显示最新的3个本地标签
+echo "Latest 3 local tags:"
+git tag -l | sort -V | tail -n 3
+
+# 显示最新的3个远程标签
+echo "Latest 3 remote tags:"
+git ls-remote --tags origin | awk '{print $2}' | sed 's/refs\/tags\///' | sort -V | tail -n 3
