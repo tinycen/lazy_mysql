6d250c26d049d329c57939c6e7864447f6a38df5
ruoce <sky_ruocen@qq.com>
2025-08-04 12:43:27 +0800

    feat(insert): 添加批量插入数据功能
    新增batch_insert方法支持批量插入数据，提高数据插入效率

--- lazy_mysql/utils/insert.py
+++ lazy_mysql/utils/insert.py
@@ -16,3 +16,29 @@ def insert(executor: SQLExecutor, table_name, insert_fields, commit=False, self_
     values = tuple(insert_fields.values())
 
     executor.execute(sql, values, commit, self_close)
+
+
+# 批量插入数据
+def batch_insert( executor: SQLExecutor, table_name, fields_list, commit=False, self_close=False ) :
+    """
+    通用的SQL批量插入执行器方法，
+    :param table_name: 表名
+    :param fields_list: 字段和值的列表，格式为字典列表，如 [{'field1': 'value1', 'field2': 'value2'}, {'field1': 'value3', 'field2': 'value4'}]
+    """
+    if not fields_list :
+        raise ValueError( "fields_list is empty !" )
+
+    # 获取字段名（假设所有字典的键相同）
+    field_names = ', '.join( fields_list[ 0 ].keys() )
+
+    # 构造占位符，例如：(%s, %s)
+    placeholders = '(' + ', '.join( [ '%s' ] * len( fields_list[ 0 ] ) ) + ')'
+
+    # 构造SQL语句
+    sql = f'''INSERT INTO {table_name} ({field_names}) VALUES {placeholders}'''
+
+    # 构造参数列表
+    values = [ tuple( item.values() ) for item in fields_list ]
+
+    # 执行SQL
+    executor.execute(sql, values, commit, self_close)
