8959a4cdef6353774499e3b933906e29ff23fb03
ruoce <sky_ruocen@qq.com>
2025-09-10 20:17:47 +0800

    up to v0.2.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except FileNotFoundError:
 
 setup(
     name='lazy_mysql',
-    version='0.2.6.3',
+    version='0.2.7',
     packages=find_packages(),
     install_requires=[
         'mysql-connector-python>=9.4.0',
