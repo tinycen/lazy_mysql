072622f01dfe46443a21cc1ecd49f310eecfbadf
tinycen <sky_ruocen@qq.com>
2026-01-02 17:36:45 +0800

    feat(__init__): 添加NDayInterval到导入列表以支持日期区间筛选

--- lazy_mysql/__init__.py
+++ lazy_mysql/__init__.py
@@ -1,10 +1,15 @@
 from .sql_config import MySQLConfig, DEFAULT_MYSQL_CONFIG
 from .executor import SQLExecutor
 from .utils import insert, upsert, select, update, delete
+from .tools.where_clause import NDayInterval
 
 __version__ = "0.1.1"
 __author__ = "tinycen"
 __email__ = "sky_ruocen@qq.com"
 
 # 提供便捷的导入
-__all__ = ['MySQLConfig', 'DEFAULT_MYSQL_CONFIG', 'SQLExecutor', 'insert', 'upsert', 'select', 'update', 'delete']
+__all__ = ['MySQLConfig', 
+           'DEFAULT_MYSQL_CONFIG',
+           'SQLExecutor','NDayInterval',
+           'insert', 'upsert', 'select',
+           'update', 'delete']
