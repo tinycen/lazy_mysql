09528b6bfb9ceee3d3595fb23284faf94f24d98a
tinycen <sky_ruocen@qq.com>
2026-05-04 16:11:22 +0800

    refactor: 更新__all__列表并移除__version__
    添加FetchConfig到__all__列表以便外部导入，同时移除不再需要的__version__变量

--- lazy_mysql/__init__.py
+++ lazy_mysql/__init__.py
@@ -2,8 +2,10 @@ from .sql_config import MySQLConfig, DEFAULT_MYSQL_CONFIG
 from .executor import SQLExecutor
 from .utils import insert, upsert, select, update, delete, merge_update_lists
 from .tools import NDayInterval, add_limit, load_sql
+from .dataclasses.fetch_config import FetchConfig
 
-__version__ = "0.4.5"
+
+
 __author__ = "tinycen"
 __email__ = "sky_ruocen@qq.com"
 
@@ -11,7 +13,7 @@ __email__ = "sky_ruocen@qq.com"
 # 提供便捷的导入
 __all__ = ['MySQLConfig', 
            'DEFAULT_MYSQL_CONFIG',
-           'SQLExecutor','NDayInterval',
+           'FetchConfig', 'NDayInterval',
            'insert', 'upsert', 'select',
            'update', 'delete', 'merge_update_lists',
            'add_limit', 'load_sql']
