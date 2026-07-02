3e7bc620f77ca6fa1f449e6e115b6973399d41d0
tinycen <sky_ruocen@qq.com>
2026-07-02 21:02:04 +0800

    fix(result_formatter): 新增dict格式的data_label非空校验
    扩展了output_format的校验范围，将dict格式加入需要校验data_label的场景，完善参数校验逻辑

--- lazy_mysql/tools/result_formatter.py
+++ lazy_mysql/tools/result_formatter.py
@@ -28,8 +28,8 @@ def fetch_format( executor , sql , fetch_mode: Literal["all", "oneTuple", "one"]
         data_label = [ ]
 
     # 验证：当输出格式为df时，data_label不能为空
-    if output_format in ["df", "df_dict"] and not data_label:
-        raise ValueError("当 output_format 为 'df' 或 'df_dict' 时，data_label 参数不能为空!")
+    if output_format in ["df", "df_dict", "dict"] and not data_label:
+        raise ValueError("当 output_format 为 'df' 或 'df_dict' 或 'dict' 时，data_label 参数不能为空!")
 
     executor.execute( sql , params , self_close = False )
 
